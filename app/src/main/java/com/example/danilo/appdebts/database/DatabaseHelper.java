c2adaa0f2d45a648771cb4c63b22cb6c6d22adf7
franciscojosecvrl <franciscojoseatdl3@gmail.com>
2019-06-26 10:26:10 -0300

    aula 26/06

---
+++ app/src/main/java/com/example/danilo/appdebts/database/DatabaseHelper.java
@@ -0,0 +1,21 @@
+package com.example.danilo.appdebts.database;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by aluno on 26/06/19.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
